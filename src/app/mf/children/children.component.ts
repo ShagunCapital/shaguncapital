67833721ce781b2c3a259e92439e1ae0a7c9c004
Subin <subinsk284@gmail.com>
2021-10-19 14:56:17 +0530

    added global css file

--- src/app/mf/children/children.component.ts
+++ src/app/mf/children/children.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {SipUtilComponent} from '../../Calculators/sip-util/sip-util.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-children',
   templateUrl: './children.component.html',
-  styleUrls: ['./children.component.css']
+  styleUrls: ['./children.component.css','../global.component.css']
 })
+
+
 export class ChildrenComponent implements OnInit {
 
   constructor() { }
@@ -12,4 +17,5 @@ export class ChildrenComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
 }
