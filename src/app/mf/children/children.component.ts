5be86bceac65c5cbde54ca54f0add8f01a2a2a4d
Harsh Singhvi <root@harshsinghvi.com>
2021-10-19 18:39:14 +0100

    Global CSS

--- src/app/mf/children/children.component.ts
+++ src/app/mf/children/children.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {SipUtilComponent} from '../../Calculators/sip-util/sip-util.component';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-children',
   templateUrl: './children.component.html',
-  styleUrls: ['./children.component.css','../global.component.css']
+  styleUrls: ['./children.component.css'],
+  // TODO: copy paste in all components
+  host: {class: 'mf'}
 })
 
 
 export class ChildrenComponent implements OnInit {
+  public readonly classList = "auth";
 
   constructor() { }
 
